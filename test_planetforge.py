c15892aa23feb84b5161410b6b2faff8d1709888
schrammer119 <95974736+schrammer119@users.noreply.github.com>
2025-07-24 19:45:06 +0200

    remove tests/ folder for better intellisense

--- test_planetforge.py
+++ test_planetforge.py
@@ -1,11 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-import sys
-import os
-
-# Add the parent directory to the path so we can import PlanetForge
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from PlanetForge import *
 
